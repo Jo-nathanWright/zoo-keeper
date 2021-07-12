4bc392f859e6901751de91aa32fc64754f3bb7ac
Jonathan Wright <j.wright72127@gmail.com>
2021-07-12 14:02:47 -0600

    Added every animal and started added traits

---
+++ app/models/Girafe.js
@@ -0,0 +1,21 @@
+class Girafe {
+    constructor(
+        name = "",
+        height = "",
+        gender = "",
+        spotNumber = 0
+    ) {
+        this.name = name;
+        this.height = height;
+        this.gender = gender;
+        this.spotNumber = spotNumber
+
+    }
+    eat(food) {
+        console.log(`Eats all the ${food}.`)
+    }
+    speak(noise) {
+        alert(`The ${this.constructor.name} says ${noise}!`)
+    }
+}
+export default Girafe;
